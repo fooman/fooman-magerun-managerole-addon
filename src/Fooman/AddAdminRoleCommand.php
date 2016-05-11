b594bb9470eca29830172f42347cc0b6cd900fca
Kristof Ringleff <kristof@fooman.co.nz>
2016-05-11 14:05:22 +0200

    Add default empty resources to role creation

--- src/Fooman/AddAdminRoleCommand.php
+++ src/Fooman/AddAdminRoleCommand.php
@@ -18,28 +18,36 @@ class AddAdminRoleCommand extends AbstractMagentoCommand
     }
 
     /**
-     * @param \Symfony\Component\Console\Input\InputInterface   $input
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @param InputInterface  $input
+     * @param OutputInterface $output
      *
-     * @return int|void
+     * @throws \Exception
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->detectMagento($output);
         if ($this->initMagento()) {
             $output->writeln('Creating role');
-            $role = \Mage::getModel('admin/role');
-            $role->setRoleName($input->getArgument('name'));
+            $name = $input->getArgument('name');
+            $role = \Mage::getModel('admin/role')->load($name, 'role_name');
+            if ($role->getId()) {
+                throw new \Exception('Role already exists: ' . $name);
+            }
+            $role->setRoleName($name);
             $role->setRoleType('G');
             $role->setPid(false);
+            $role->setTreeLevel(1);
             try {
                 $role->save();
+                //start with empty permissions only
+                \Mage::getModel('admin/rules')
+                    ->setRoleId($role->getId())
+                    ->setResources(array(''))
+                    ->saveRel();
             } catch (\Exception $e) {
                 $output->writeln('Error creating role ' . $e->getMessage());
-
             }
             $output->writeln('Created role ' . $input->getArgument('name'));
-
         }
     }
 }
